9afe0f81ff5af2fa39bceac9402b902d69c211d8
DESKTOP-ICDA5E5\Korisnik <nevena.peric@student.etf.unibl.org>
2024-01-09 21:02:15 +0100

    3.10 Registrovanje ponude

--- Korisnik.cpp
+++ Korisnik.cpp
@@ -1,23 +1,21 @@
 #include "Korisnik.h"
 
-void Korisnik::slanje_ponude()
+int Korisnik::slanje_ponude()
 {
-    std::string id;
     std::string tipN; // Tip nekretnine (npr. "Stan", "Kuća", "Poslovni prostor")
     std::string adresa;
-    std::string vlasnik;
     double povrsina;
     int brojSoba;
     std::string opis;
     double cena;
-    bool dostupnost;
+
     std::cout << "Unesite adresu nekretnine: ";
     std::cin.ignore(); // Da se izbegne problem sa preostalim newline karakterom u baferu
     std::getline(std::cin, adresa);
     if (adresa.empty())
     {
         std::cout << "Adresa ne sme biti prazna. Molimo vas ponovite unos." << std::endl;
-        return;
+       
     }
 
     std::cout << "Unesite povrsinu nekretnine (m^2): ";
@@ -25,7 +23,7 @@ void Korisnik::slanje_ponude()
     if (povrsina <= 0)
     {
         std::cout << "Povrsina mora biti pozitivan broj. Molimo vas ponovite unos." << std::endl;
-        return;
+        
     }
 
     std::cout << "Unesite broj soba nekretnine: ";
@@ -33,7 +31,7 @@ void Korisnik::slanje_ponude()
     if (brojSoba <= 0)
     {
         std::cout << "Broj soba mora biti pozitivan broj. Molimo vas ponovite unos." << std::endl;
-        return;
+        
     }
     std::cin.ignore(); // Da se izbegne problem sa preostalim newline karakterom u baferu
 
@@ -42,8 +40,21 @@ void Korisnik::slanje_ponude()
     if (tipN.empty())
     {
         std::cout << "Tip nekretnine ne sme biti prazan. Molimo vas ponovite unos." << std::endl;
-        return;
     }
+
+    std::cout << "Unesite opis nekretnine: ";
+    std::cin.ignore(); // Da se izbegne problem sa preostalim newline karakterom u baferu
+    std::getline(std::cin, opis);
+
+    std::cout << "Prodajna cijena nekretnine ce biti vece za 5% u odnosu na unesenu cijenu - Unesite cijenu nekretnine: ";
+    std::cin >> cena;
+    if (cena <= 0)
+    {
+        std::cout << "Cena mora biti pozitivan broj. Molimo vas ponovite unos." << std::endl;
+        
+    }
+    else
+    cena=cena+(cena/100*5);
 
     std::cout << "Automatski unos informacija o vlasniku." << std::endl;
     // Ovdje biste mogli implementirati kod za automatski unos informacija o vlasniku
@@ -53,8 +64,7 @@ void Korisnik::slanje_ponude()
     std::cin >> odgovor;
     if (odgovor == "Da" || odgovor == "da")
     {
-
-        std::ofstream datoteka("poslate_nekretnine.txt", std::ios::app); // Otvori datoteku za pisanje, dodajte informacije na kraju
+        std::ofstream datoteka("poslate_ponude.txt", std::ios::app); // Otvori datoteku za pisanje, dodajte informacije na kraju
 
         if (datoteka.is_open())
         {
@@ -62,7 +72,13 @@ void Korisnik::slanje_ponude()
             datoteka << adresa << ' ';
             datoteka << povrsina << ' ';
             datoteka << brojSoba << ' ';
-            datoteka << tipN << std::endl;
+            datoteka << tipN << ' ';
+            datoteka << opis << ' ';
+            datoteka << cena << std::endl;
+
+
+            
+
             std::cout << "Ponuda je uspjesno poslana." << std::endl;
 
             datoteka.close(); // Zatvori datoteku nakon upisa
@@ -76,4 +92,46 @@ void Korisnik::slanje_ponude()
     {
         std::cout << "Informacije nisu sačuvane." << std::endl;
     }
+    if(odgovor=="DA" || odgovor=="Da" || odgovor=="da")
+    return 1;
+    else return 0;
+}
+
+void Korisnik::registrovanje_ponude()
+{
+    std::ifstream inputFile("poslate_ponude.txt"); // Zamenite "nekretnine.txt" sa imenom vaše datoteke
+
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Nije moguće prikazati listu ponuda." << std::endl;
+    }
+
+    std::string line1;
+    std::string line2;
+    int redniBroj = 1;
+
+   while (std::getline(inputFile, line1) && std::getline(inputFile, line2))
+    {
+        std::cout << redniBroj <<' '<<line1<<std::endl;
+        std::cout<< line2  << std::endl;
+        redniBroj++;
+    }
+    
+
+    inputFile.close();
+}
+
+
+void Korisnik::automatski_unos()
+{
+    std::ofstream datoteka("poslate_ponude.txt", std::ios::app); // Otvori datoteku za pisanje, dodajte informacije na kraju
+
+        if (datoteka.is_open())
+        {
+            datoteka<<ime<<' ';
+            datoteka<<prezime<<' ';
+            datoteka<<brojTelefona<<' ';
+            datoteka<<emailAdresa<<std::endl;
+            datoteka<<tipProfila<<std::endl;
+        }
 }
--- Korisnik.h
+++ Korisnik.h
@@ -9,8 +9,8 @@
 #include <sstream>
 #include <string>
 
-
-class Korisnik {
+class Korisnik
+{
 private:
     std::string korisnickoIme;
     std::string sifra;
@@ -22,91 +22,67 @@ private:
 
 public:
     Korisnik()
-        : korisnickoIme(""), sifra(""), tipProfila(""), ime(""), prezime(""), brojTelefona(""), emailAdresa("") {
+        : korisnickoIme(""), sifra(""), tipProfila(""), ime(""), prezime(""), brojTelefona(""), emailAdresa("")
+    {
     }
 
     Korisnik(std::string korIme, std::string sif, std::string tipProf, std::string im, std::string prez, std::string telefon, std::string email)
-        : korisnickoIme(korIme), sifra(sif), tipProfila(tipProf), ime(im), prezime(prez), brojTelefona(telefon), emailAdresa(email) {
+        : korisnickoIme(korIme), sifra(sif), tipProfila(tipProf), ime(im), prezime(prez), brojTelefona(telefon), emailAdresa(email)
+    {
     }
 
     void registrujSe()
-{
-    std::cout << " \n [1] Vlasnik nekretnine,\n [2] Kupac nekretnine,\n [3] Iznajmljivac nekretnine,\n [4] Radnik agencije,\n [5] Odrzavatelj,\n";
-    std::cout<<"\n> Odaberite tip profila: ";
-    std::cin>>tipProfila;
-    int br=0;
-
-    if (tipProfila == "1" || tipProfila == "[1] Vlasnik nekretnine" || tipProfila == "[1]" || tipProfila == "Vlasnik nekretnine" || tipProfila == "vlasnik nekretnine" || tipProfila == "Vlasnik" || tipProfila == "vlasnik")
     {
-        tipProfila = "1";
-    }
+        std::cout << " \n [1] Vlasnik nekretnine,\n [2] Kupac nekretnine,\n [3] Iznajmljivac nekretnine,\n [4] Radnik agencije,\n [5] Odrzavatelj,\n";
+        std::cout << "\n> Odaberite tip profila: ";
+        std::cin >> tipProfila;
+        int br = 0;
 
-    else if (tipProfila == "2" || tipProfila == "[2] Kupac nekretnine" || tipProfila == "[2]" || tipProfila == "Kupac nekretnine" || tipProfila == "kupac nekretnine" || tipProfila == "Kupac" || tipProfila == "kupac")
-    {
-        tipProfila = "2";
-    }
+        if (tipProfila == "1" || tipProfila == "[1] Vlasnik nekretnine" || tipProfila == "[1]" || tipProfila == "Vlasnik nekretnine" || tipProfila == "vlasnik nekretnine" || tipProfila == "Vlasnik" || tipProfila == "vlasnik")
+        {
+            tipProfila = "1";
+        }
 
-    else if (tipProfila == "3" || tipProfila == "[3] Iznajmljivac nekretnine" || tipProfila == "[3]" || tipProfila == "Iznajmljivac nekretnine" || tipProfila == "Iznajmljivac nekretnine" || tipProfila == "Iznajmljivac" || tipProfila == "iznajmljivac")
-    {
-        tipProfila = "3";
-    }
+        else if (tipProfila == "2" || tipProfila == "[2] Kupac nekretnine" || tipProfila == "[2]" || tipProfila == "Kupac nekretnine" || tipProfila == "kupac nekretnine" || tipProfila == "Kupac" || tipProfila == "kupac")
+        {
+            tipProfila = "2";
+        }
 
-    else if (tipProfila == "4" || tipProfila == "[4] Radnik agencije" || tipProfila == "[4]" || tipProfila == "Radnik agencije" || tipProfila == "radnik agencije" || tipProfila == "Radnik" || tipProfila == "radnik")
-    {
-        tipProfila = "4";
-    }
+        else if (tipProfila == "3" || tipProfila == "[3] Iznajmljivac nekretnine" || tipProfila == "[3]" || tipProfila == "Iznajmljivac nekretnine" || tipProfila == "Iznajmljivac nekretnine" || tipProfila == "Iznajmljivac" || tipProfila == "iznajmljivac")
+        {
+            tipProfila = "3";
+        }
 
-    else if (tipProfila == "5" || tipProfila == "[5] Odrzavatelj" || tipProfila == "[5]" || tipProfila == "Odrzavatelj" || tipProfila == "odrzavatelj")
-    {
-        tipProfila = "5";
-    }
+        else if (tipProfila == "4" || tipProfila == "[4] Radnik agencije" || tipProfila == "[4]" || tipProfila == "Radnik agencije" || tipProfila == "radnik agencije" || tipProfila == "Radnik" || tipProfila == "radnik")
+        {
+            tipProfila = "4";
+        }
 
-    else {
-        std::cout<<"... Tip profila nije adekvatno odabran, pokusajte ponovo.\n";
-        return;
-    }
+        else if (tipProfila == "5" || tipProfila == "[5] Odrzavatelj" || tipProfila == "[5]" || tipProfila == "Odrzavatelj" || tipProfila == "odrzavatelj")
+        {
+            tipProfila = "5";
+        }
 
-    std::cout << "> Unesite korisnicko ime: ";
-    std::cin >> korisnickoIme;
+        else
+        {
+            std::cout << "... Tip profila nije adekvatno odabran, pokusajte ponovo.\n";
+            return;
+        }
 
-    while (proveriPostojanjeKorisnickogImena())
-    {
-        std::cout << "... Korisnicko ime vec postoji. Molimo Vas da odaberete drugo korisnicko ime.\n";
+        std::cout << "> Unesite korisnicko ime: ";
         std::cin >> korisnickoIme;
-    }
 
-    std::string sifra;
-    std::string ponovljenaSifra;
-    char karakter;
-
-    std::cout << "> Unesite lozinku (minimalno 8 karaktera): ";
-
-    while (true)
-    {
-        karakter = _getch();
-
-        if (karakter == 13)
+        while (proveriPostojanjeKorisnickogImena())
         {
-            break;
+            std::cout << "... Korisnicko ime vec postoji. Molimo Vas da odaberete drugo korisnicko ime.\n";
+            std::cin >> korisnickoIme;
         }
 
-        if (karakter == 8 && !sifra.empty())
-        {
-            std::cout << "\b \b";
-            sifra.pop_back();
-        }
-        else if (karakter != 8)
-        {
-            sifra.push_back(karakter);
-            std::cout << '*';
-        }
-    }
+        std::string sifra;
+        std::string ponovljenaSifra;
+        char karakter;
 
-    while (sifra.length() < 8)
-    {
-        std::cout << "\n... Lozinka mora imati barem 8 karaktera. Unesite ponovo lozinku: ";
-
-        sifra.clear();
+        std::cout << "> Unesite lozinku (minimalno 8 karaktera): ";
 
         while (true)
         {
@@ -128,172 +104,226 @@ public:
                 std::cout << '*';
             }
         }
+
+        while (sifra.length() < 8)
+        {
+            std::cout << "\n... Lozinka mora imati barem 8 karaktera. Unesite ponovo lozinku: ";
+
+            sifra.clear();
+
+            while (true)
+            {
+                karakter = _getch();
+
+                if (karakter == 13)
+                {
+                    break;
+                }
+
+                if (karakter == 8 && !sifra.empty())
+                {
+                    std::cout << "\b \b";
+                    sifra.pop_back();
+                }
+                else if (karakter != 8)
+                {
+                    sifra.push_back(karakter);
+                    std::cout << '*';
+                }
+            }
+        }
+
+        std::cout << "\n> Ponovite lozinku: ";
+
+        while (true)
+        {
+            karakter = _getch();
+
+            if (karakter == 13)
+            {
+                if (sifra.length() >= 8 && ponovljenaSifra == sifra)
+                {
+                    break;
+                }
+                else
+                {
+                    std::cout << "\n... Lozinke se ne podudaraju ili nisu ispunjeni uslovi. Molimo Vas da ponovo unesete lozinku: ";
+                    ponovljenaSifra.clear();
+                    continue;
+                }
+            }
+
+            if (karakter == 8 && !ponovljenaSifra.empty())
+            {
+                std::cout << "\b \b";
+                ponovljenaSifra.pop_back();
+            }
+            else if (karakter != 8)
+            {
+                ponovljenaSifra.push_back(karakter);
+                std::cout << '*';
+            }
+        }
+
+        std::cout << "\n> Unesite ime: ";
+        std::cin >> ime;
+
+        std::cout << "> Unesite prezime: ";
+        std::cin >> prezime;
+
+        std::cout << "> Unesite broj telefona: ";
+        std::cin >> brojTelefona;
+
+        std::cout << "> Unesite e-mail adresu: ";
+        std::cin >> emailAdresa;
+
+        std::cout << "... Registracija uspjesna!\n";
+
+        std::ofstream file("nalozi.txt", std::ios::app);
+        file << tipProfila << " " << korisnickoIme << " " << sifra << " " << ime << " " << prezime << " " << brojTelefona << " " << emailAdresa << "\n";
+        file.close();
     }
 
-    std::cout << "\n> Ponovite lozinku: ";
-
-    while (true)
+    bool proveriPostojanjeKorisnickogImena() const
     {
-        karakter = _getch();
+        std::ifstream file("nalozi.txt");
+        std::string line;
 
-        if (karakter == 13)
+        while (std::getline(file, line))
         {
-            if (sifra.length() >= 8 && ponovljenaSifra == sifra)
+            std::istringstream iss(line);
+            std::string existingUsername;
+
+            if (!(iss >> existingUsername))
+            {
+
+                continue;
+            }
+
+            if (existingUsername == korisnickoIme)
+            {
+                file.close();
+                return true;
+            }
+        }
+
+        file.close();
+        return false;
+    }
+    void prijaviSe()
+    {
+        std::cout << "~ Prijava na sistem ~\n";
+        std::cout << "Korisnicko ime: ";
+        std::cin >> korisnickoIme;
+
+        std::string Sifra;
+        std::cout << "Lozinka: ";
+        char karakter;
+
+        while (true)
+        {
+            karakter = _getch();
+
+            if (karakter == 13)
             {
                 break;
             }
-            else
+
+            if (karakter == 8 && !Sifra.empty())
             {
-                std::cout << "\n... Lozinke se ne podudaraju ili nisu ispunjeni uslovi. Molimo Vas da ponovo unesete lozinku: ";
-                ponovljenaSifra.clear();
+                std::cout << "\b \b";
+                Sifra.pop_back();
+            }
+            else if (karakter != 8)
+            {
+                Sifra.push_back(karakter);
+                std::cout << '*';
+            }
+        }
+
+        std::ifstream file("nalozi.txt");
+        std::string line;
+
+        while (std::getline(file, line))
+        {
+            std::istringstream iss(line);
+            std::string pr, ki, si, i, p, bt, ea;
+
+            if (!(iss >> pr >> ki >> si >> i >> p >> bt >> ea))
+            {
                 continue;
             }
-        }
 
-        if (karakter == 8 && !ponovljenaSifra.empty())
-        {
-            std::cout << "\b \b";
-            ponovljenaSifra.pop_back();
-        }
-        else if (karakter != 8)
-        {
-            ponovljenaSifra.push_back(karakter);
-            std::cout << '*';
-        }
-    }
+            // uspjesna prijava---------------------------------------------------------------------------------------
+            if (ki == korisnickoIme && si == Sifra)
+            {
+                file.close();
 
-    std::cout << "\n> Unesite ime: ";
-    std::cin >> ime;
+                std::string ind2;
 
-    std::cout << "> Unesite prezime: ";
-    std::cin >> prezime;
+                std::cout << "\n\nUspesno ste se prijavili!\n\n\n";
 
-    std::cout << "> Unesite broj telefona: ";
-    std::cin >> brojTelefona;
+                Korisnik k(ki, si, pr, i, p, bt, ea);
 
-    std::cout << "> Unesite e-mail adresu: ";
-    std::cin >> emailAdresa;
+                k.prikazProfila();
 
-    std::cout << "... Registracija uspjesna!\n";
+                if (pr == "1")
+                    std::cout << "\nDostupne operacije vlasniku: [PRIKAZ], [SLANJE], [ODJAVA] ....\n";
+                if (pr == "2")
+                    std::cout << "\nDostupne operacije kupac: [PRIKAZ], [APLICIRANJE], [ODJAVA] ....\n";
+                if (pr == "3")
+                    std::cout << "\nDostupne operacije iznajmljivac: [PRIKAZ], [APLICIRANJE], [ODJAVA] ....\n";
+                if (pr == "4")
+                    std::cout << "\nDostupne operacije radnik agencije: [PRIKAZ], [REGISTROVANJE], [GENERISANJE], [ODJAVA]....\n";
+                if (pr == "5")
+                    std::cout << "\nDostupne operacije odrzavatelj: [PRIKAZ], [ZADACI], [AZADATAK], [ODJAVA]....\n";
 
-    std::ofstream file("nalozi.txt", std::ios::app);
-    file << tipProfila << " " << korisnickoIme << " " << sifra << " " << ime << " " << prezime << " " << brojTelefona << " " << emailAdresa << "\n";
-    file.close();
-}
+                while (ind2 != "ODJAVA")
+                {
+                    // prikaz informacija o transakcijama-----------------------------------------------
 
-bool proveriPostojanjeKorisnickogImena() const
-{
-    std::ifstream file("nalozi.txt");
-    std::string line;
+                    std::cout << "\n> ";
+                    std::cin >> ind2;
+                    if (ind2 == "PRIKAZ")
+                        std::cout << "\nTrenutno dostupne nekretnine.\n";
+                    else if (ind2 == "PRIJAVA")
+                        std::cout << "\nTrenutno ste vec prijavljeni.\n";
+                    else if (ind2 == "SLANJE" && pr == "1")
+                    {
+                        int tmp;
+                        tmp = slanje_ponude();
+                        if (tmp)
+                            k.automatski_unos();
+                    }
+                    else if (ind2 == "REGISTROVANJE" && pr == "4")
+                        registrovanje_ponude();
+                    else
+                    {
+                        if (ind2 != "ODJAVA")
+                            std::cout << "\nTa opcija nije dostupna.\n";
+                    }
 
-    while (std::getline(file, line))
-    {
-        std::istringstream iss(line);
-        std::string existingUsername;
-
-        if (!(iss >> existingUsername))
-        {
-
-            continue;
-        }
-
-        if (existingUsername == korisnickoIme)
-        {
-            file.close();
-            return true;
-        }
-    }
-
-    file.close();
-    return false;
-}
-    void prijaviSe() {
-    std::cout << "~ Prijava na sistem ~\n";
-    std::cout << "Korisnicko ime: ";
-    std::cin >> korisnickoIme;
-
-    std::string Sifra;
-    std::cout << "Lozinka: ";
-    char karakter;
-
-    while (true) {
-        karakter = _getch();
-
-        if (karakter == 13) {
-            break;
-        }
-
-        if (karakter == 8 && !Sifra.empty()) {
-            std::cout << "\b \b";
-            Sifra.pop_back();
-        } else if (karakter != 8) {
-            Sifra.push_back(karakter);
-            std::cout << '*';
-        }
-    }
-
-    std::ifstream file("nalozi.txt");
-    std::string line;
-
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string pr, ki, si, i, p, bt, ea;
-
-        if (!(iss >> pr >> ki >> si >> i >> p >> bt >> ea)) {
-            continue;
-        }
-
-        // uspjesna prijava---------------------------------------------------------------------------------------
-        if (ki == korisnickoIme && si == Sifra) {
-            file.close();
-
-            std::string ind2;
-
-            std::cout << "\n\nUspesno ste se prijavili!\n\n\n";
-
-            Korisnik k(ki, si, pr, i, p, bt, ea);
-
-            k.prikazProfila();
-
-            if(pr=="1")std::cout<<"\nDostupne operacije vlasniku: [PRIKAZ], [SLANJE], [ODJAVA] ....\n";
-            if(pr=="2")std::cout<<"\nDostupne operacije kupac: [PRIKAZ], [APLICIRANJE], [ODJAVA] ....\n";
-            if(pr=="3")std::cout<<"\nDostupne operacije iznajmljivac: [PRIKAZ], [APLICIRANJE], [ODJAVA] ....\n";
-            if(pr=="4")std::cout<<"\nDostupne operacije radnik agencije: [PRIKAZ], [REGISTROVANJE], [GENERISANJE], [ODJAVA]....\n";
-            if(pr=="5")std::cout<<"\nDostupne operacije odrzavatelj: [PRIKAZ], [ZADACI], [AZADATAK], [ODJAVA]....\n";
-
-
-            while(ind2!="ODJAVA"){
-                //prikaz informacija o transakcijama-----------------------------------------------
-
-                std::cout<<"\n> ";
-                std::cin>>ind2;
-                if(ind2=="PRIKAZ") std::cout<<"\nTrenutno dostupne nekretnine.\n";
-                else if(ind2=="PRIJAVA") std::cout<<"\nTrenutno ste vec prijavljeni.\n";
-                else if(ind2=="SLANJE" && pr=="1")
-                slanje_ponude();
-                else{
-                    if(ind2!="ODJAVA")std::cout<<"\nTa opcija nije dostupna.\n";
+                    /*
+                        U ZAVISNOSTI OD PROFILA I DOSTUPNOSTI ISTOG, PRIKAZUJU SE OPCIJE KORISNICIMA
+                    */
                 }
-              
-                /*
-                    U ZAVISNOSTI OD PROFILA I DOSTUPNOSTI ISTOG, PRIKAZUJU SE OPCIJE KORISNICIMA
-                */
+                return;
             }
-            return;
+
+            else if (ki == korisnickoIme)
+            {
+                file.close();
+                std::cout << "\nPogresna lozinka. Pokusajte ponovo.\n";
+                return;
+            }
         }
 
-        else if (ki == korisnickoIme) {
-            file.close();
-            std::cout << "\nPogresna lozinka. Pokusajte ponovo.\n";
-            return;
-        }
+        file.close();
+        std::cout << "\nKorisnicko ime nije pronadjeno. Molimo vas da se registrujete.\n";
     }
-
-    file.close();
-    std::cout << "\nKorisnicko ime nije pronadjeno. Molimo vas da se registrujete.\n";
-}
     void prikazProfila();
     void odjava();
-    void slanje_ponude();
-
+    int slanje_ponude();
+    void registrovanje_ponude();
+    void automatski_unos();
 };
